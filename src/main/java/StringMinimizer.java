4fb001088e4c741dcf0cf3a59edbc0a68fc565d3
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-07-24 17:43:57 +0100

    get duplicate words

--- src/main/java/StringMinimizer.java
+++ src/main/java/StringMinimizer.java
@@ -5,19 +5,31 @@ public class StringMinimizer {
 //	look for duplicate words (identifiers) and replace duplicate with $index of first seen word
 //	identifier starts at 0
 //	output entire string with minimized changes
-
+	
 	//	string for input
-	static String string = "you say yes, I say no you say stop and I say go go go";
+	static String string = "You say you yes, I say no you say stop and I say go go go";
+	static int count;
 
-	static String[] minimize(String input) {
+	public static String[] minimize(String input) {
+//split the input string to get each single word
 		String[] word = input.split(" ");
-		return word;
+//loop through to get duplicate words
+		for (int i = 0; i < word.length; i++) {
+			count = 1;
+			for (int j = i + 1; j < word.length; j++) {
+				if(word[i].equals(word[j])) {
+					count++;
+				}
+			}
+			if (count > 1) {
+				System.out.println(word[i]);
+			}
+		}return word;
 	}
 
 	public static void main(String[] args) {
 //		call to minimize function
-		final String[] minimize = minimize(string);
-		System.out.println(minimize[1]);
+		minimize(string);
 	}
 
 }
