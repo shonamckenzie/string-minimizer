2ce00adc29bb78824786161e00ea6b5146eb8def
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-07-25 14:26:56 +0100

    add index position

--- src/main/java/StringMinimizer.java
+++ src/main/java/StringMinimizer.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringMinimizer {
 
 //	minimizer function to take string as input
@@ -5,31 +7,31 @@ public class StringMinimizer {
 //	look for duplicate words (identifiers) and replace duplicate with $index of first seen word
 //	identifier starts at 0
 //	output entire string with minimized changes
-	
-	//	string for input
-	static String string = "You say you yes, I say no you say stop and I say go go go";
-	static int count;
 
-	public static String[] minimize(String input) {
+	//	string for input
+	static String string = "you say yes, I say no you say stop and I say go go go";
+	static String[] word = string.split(" ");
+	static int index;
+
+	public static String minimize(String[] word) {
 //split the input string to get each single word
-		String[] word = input.split(" ");
 //loop through to get duplicate words
 		for (int i = 0; i < word.length; i++) {
-			count = 1;
+			index = i;
 			for (int j = i + 1; j < word.length; j++) {
 				if(word[i].equals(word[j])) {
-					count++;
+					word[j] = "$" + index;
 				}
 			}
-			if (count > 1) {
-				System.out.println(word[i]);
-			}
-		}return word;
+//				System.out.println(count);
+		}System.out.println(Arrays.toString(word));
+		return Arrays.toString(word);
+
 	}
 
 	public static void main(String[] args) {
 //		call to minimize function
-		minimize(string);
+		minimize(word);
 	}
 
 }
--- src/test/java/TestStringMinimizer.java
+++ src/test/java/TestStringMinimizer.java
@@ -6,15 +6,18 @@ import static org.junit.Assert.assertEquals;
 public class TestStringMinimizer {
 
 	StringMinimizer minimizer;
+	String string = "you say yes, I say no you say stop and I say go go go";
+	String[] word = string.split(" ");
 
 	@Before
 	public void setup() throws Exception {
 		minimizer = new StringMinimizer();
+
 	}
 
 	@Test
 	public void testStringMinimizer() {
-		assertEquals("you say yes, I $1 no $0 $1 stop and $3 $1 go $12 $12", minimizer.minimize("You say you yes, I say no you say stop and I say go go go"));
+		assertEquals("you say yes, I $1 no $0 $1 stop and $3 $1 go $12 $12", minimizer.minimize(word));
 	}
 
 }
